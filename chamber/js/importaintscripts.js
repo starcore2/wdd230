2fe1480eaf66e4431a7f47e55e328f1975d11d29
starcore2 <liam@zacher.us>
2022-01-29 19:19:44 -0600

    Testing if the date updated script works

---
+++ chamber/js/importaintscripts.js
@@ -0,0 +1,7 @@
+var lastohmosa = document.lastModified;
+
+timetomod = document.querySelector('#datelast').textContent 
+
+timetomod = timetomod + lastohmosa 
+
+document.querySelector('#datelast').textContent = timetomod
