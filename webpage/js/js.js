38f0c2716029d2fbd967cd99309581d58eb4abea
starcore2 <liam@zacher.us>
2022-01-29 19:10:30 -0600

    Lots of chages and a fix for a problem

--- webpage/js/js.js
+++ webpage/js/js.js
@@ -1,4 +1,7 @@
-function getthedatelast(){
-    var lastohmosa = document.lastModified;
-    return lastohmosa
-}
+var lastohmosa = document.lastModified;
+
+timetomod = document.querySelector('#datelast').textContent 
+
+timetomod = timetomod + lastohmosa 
+
+document.querySelector('#datelast').textContent = timetomod
