a130cc2b8ae467c1e0ede997c01b9cf54b6e5faa
starcore2 <liam@zacher.us>
2022-01-29 19:46:35 -0600

    forgot to add semicolon

--- chamber/js/importaintscripts.js
+++ chamber/js/importaintscripts.js
@@ -1,7 +1,7 @@
 var lastohmosa = document.lastModified;
 
-timetomod = document.querySelector('#datelast').textContent 
+timetomod = document.querySelector('#datelast').textContent;
 
-timetomod = timetomod + lastohmosa 
+timetomod = timetomod + lastohmosa;
 
-document.querySelector('#datelast').textContent = timetomod
+document.querySelector('#datelast').textContent = timetomod;
